18acf5c6cd55f7099b4f197603f9dad88ceae011
Prathamesh Bhat <prathameshtengse25@gmail.com>
2024-06-16 13:03:11 +0530

    register,login page

--- server/index.js
+++ server/index.js
@@ -11,28 +11,26 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import { createPost } from "./controllers/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
-
-const __filename = fileURLToPath(import.meta.url); //It represents the absolute path of the current module file.
-const __dirname = path.dirname(__filename); //It represents the absolute path of the directory containing the current module file.
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet()); //for security
+app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets"))); //sets up a route that serves as static files
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -40,13 +38,13 @@ const storage = multer.diskStorage({
     cb(null, "public/assets");
   },
   filename: function (req, file, cb) {
-    cb(null, "public/assets");
+    cb(null, file.originalname);
   },
 });
 const upload = multer({ storage });
 
-/* ROUTES WITH FILES*/
-app.post("/auth/register", upload.single("picture"), register); //uploads pictures locally
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
@@ -54,16 +52,17 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-/* MONGOOSE SETUP*/
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true, //is for using the new URL parser
-    useUnifiedTopology: true, // is for using the new Server Discovery and Monitoring engine.
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Post: ${PORT}`));
-    /*ADD ONE TIME */
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
